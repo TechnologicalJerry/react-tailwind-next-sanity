e033e2828aa6cdf0c1a1ec6553862cba29b5c716
technologicaljerry <cserajatverma@gmail.com>
2023-04-20 21:20:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>This is home page for testing</h1>
+        <Link href={'/dashboard'}>Go to the Dashboard</Link>
     </main>
   )
 }
