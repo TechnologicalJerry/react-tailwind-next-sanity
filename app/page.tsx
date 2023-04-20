d2698be6b17d06906c1f8ca26bb90093ec3ffead
technologicaljerry <cserajatverma@gmail.com>
2023-04-20 21:25:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="py-8 px-48">
-        <Link href={'/dashboard'}>Go to the Dashboard</Link>
+        <Link className=' bg-teal-500 text-black font-medium py-2 px-4 rounded-md'
+        href={'/dashboard'}>
+          Go to the Dashboard
+          </Link>
     </main>
   )
 }
