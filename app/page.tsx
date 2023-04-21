00fee1b4ae0cd39c5bd05a538827af513ad01062
technologicaljerry <cserajatverma@gmail.com>
2023-04-21 23:46:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="py-8 px-48">
       <Link className=' bg-teal-500 text-black font-medium py-2 px-4 rounded-md'
         href={'/dashboard'}>
-        Go to the Dashboard
+        Dashboard
       </Link>
     </main>
   )
