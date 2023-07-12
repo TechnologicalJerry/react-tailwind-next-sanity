253b939504be05811f58c4401eb07d14aabf5d64
technologicaljerry <cserajatverma@gmail.com>
2023-07-12 23:49:36 +0530

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
                 <div className="flex justify-between items-center w-full">
-
+                    <Link href="/">
+                        <h1 className="text-2x1 font-medium">
+                            Jan <span className="text-teal-500">Blog</span>
+                        </h1>
+                    </Link>
                 </div>
             </div>
         </div>
