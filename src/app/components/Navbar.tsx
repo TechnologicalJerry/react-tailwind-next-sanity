11d1a215478e0247f142f2eabc23b1a5b5c7d892
technologicaljerry <cserajatverma@gmail.com>
2023-07-12 23:44:42 +0530

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,9 @@
 export default function Navbar(){
     return(
         <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-          
+            <div className="flex justify-between h-16">
+
+            </div>
         </div>
     )
 }
