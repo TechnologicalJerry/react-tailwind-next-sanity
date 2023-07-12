0353d44146ba83c747f0ca76a93a4e1c99869971
technologicaljerry <cserajatverma@gmail.com>
2023-07-12 23:20:42 +0530

    updates

--- app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function About() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>This is About page</h1>
-        </main>
-    )
-}
