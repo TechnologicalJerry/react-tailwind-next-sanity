3343435cca19b6cebbadbecb758ee7e886ca5624
technologicaljerry <cserajatverma@gmail.com>
2023-07-14 23:46:13 +0530

    Update ThemeButton.tsx

--- src/app/components/ThemeButton.tsx
+++ src/app/components/ThemeButton.tsx
@@ -6,4 +6,10 @@ import { useEffect, useState } from "react";
 export default function Themebutton() {
     const { setTheme, resolvedTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    
 }
