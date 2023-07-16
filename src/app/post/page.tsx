6190ca1e057e6ae02b0b3dcd177eb1e2066b10ca
technologicaljerry <cserajatverma@gmail.com>
2023-07-16 23:39:11 +0530

    Update page.tsx

---
+++ src/app/post/page.tsx
@@ -0,0 +1,5 @@
+import { Post } from "@/app/lib/interface";
+import { client } from "@/app/lib/sanity";
+import { urlFor } from "@/app/lib/sanityImageUrl";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
